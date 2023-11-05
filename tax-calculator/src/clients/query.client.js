4a6d844b19507abd1a54fe4ef9b55c5d1ec5aa8a
praveenkumarct <praveen.kumar@commercetools.com>
2023-11-05 21:17:13 +0100

    Tax calculator basic code to get calculation from stripe

--- tax-calculator/src/clients/query.client.js
+++ tax-calculator/src/clients/query.client.js
@@ -1,60 +1,26 @@
 import { createApiRoot } from './create.client.js';
 import CustomError from '../errors/custom.error.js';
-import { HTTP_STATUS_BAD_REQUEST } from '../constants/http.status.constants.js';
+import { HTTP_STATUS_RESOURCE_NOT_FOUND } from '../constants/http.status.constants.js';
 
-const CHUNK_SIZE = 100;
 const queryArgs = {
-  limit: CHUNK_SIZE,
   withTotal: false,
-  sort: 'product.id asc',
-  expand: ['productSelection', 'taxCategory', 'productType', 'categories[*]'],
+  expand: ['cart'],
 };
 
-export async function getProductProjectionInStoreById(storeKey, productId) {
+export async function getCartByOrderId(orderId) {
   return await createApiRoot()
-    .inStoreKeyWithStoreKeyValue({
-      storeKey: Buffer.from(storeKey).toString(),
-    })
-    .productProjections()
-    .withId({
-      ID: Buffer.from(productId).toString(),
-    })
-    .get({ queryArgs })
-    .execute()
-    .then((response) => response.body);
-}
-
-export async function getProductsInCurrentStore(storeKey) {
-  let lastProductId = undefined;
-  let hasNextQuery = true;
-  let allProducts = [];
-
-  while (hasNextQuery) {
-    if (lastProductId) {
-      queryArgs.where = `product(id>"${lastProductId}")`;
-    }
-
-    let productChunk = await createApiRoot()
-      .inStoreKeyWithStoreKeyValue({
-        storeKey: Buffer.from(storeKey).toString(),
+      .orders()
+      .withId({
+        ID: Buffer.from(orderId).toString(),
       })
-      .productSelectionAssignments()
       .get({ queryArgs })
       .execute()
-      .then((response) => response.body.results)
-      .then((results) => results.map((result) => result.product))
+      .then((response) => response.body?.cart.obj)
       .catch((error) => {
         throw new CustomError(
-          HTTP_STATUS_BAD_REQUEST,
-          `Bad request: ${error.message}`,
-          error
+            HTTP_STATUS_RESOURCE_NOT_FOUND,
+            `Bad request: ${error.message}`,
+            error
         );
       });
-    hasNextQuery = productChunk.length == CHUNK_SIZE;
-    if (productChunk.length > 0) {
-      lastProductId = productChunk[productChunk.length - 1].id;
-      allProducts = allProducts.concat(productChunk);
-    }
-  }
-  return allProducts;
 }
