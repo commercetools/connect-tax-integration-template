3c0334fae755b3ed183b59dff2c9b71103ebac6c
King-Hin Leung <kinghin.leung@commercetools.com>
2023-11-14 14:34:38 +0100

    Avoid using env-var for subscription key

--- order-syncer/src/connectors/pre-undeploy.js
+++ order-syncer/src/connectors/pre-undeploy.js
@@ -2,18 +2,14 @@ import { createApiRoot } from '../clients/create.client.js';
 import { CTP_ORDER_CHANGE_SUBSCRIPTION_KEY } from '../constants/connectors.constants.js';
 import { deleteChangedOrderSubscription } from './actions.js';
 
-async function preUndeploy(properties) {
+async function preUndeploy() {
   const apiRoot = createApiRoot();
-  const ctpOrderChangeSubscriptionKey = properties.get(
-    CTP_ORDER_CHANGE_SUBSCRIPTION_KEY
-  );
-  await deleteChangedOrderSubscription(apiRoot, ctpOrderChangeSubscriptionKey);
+  await deleteChangedOrderSubscription(apiRoot, CTP_ORDER_CHANGE_SUBSCRIPTION_KEY);
 }
 
 async function run() {
   try {
-    const properties = new Map(Object.entries(process.env));
-    await preUndeploy(properties);
+    await preUndeploy();
   } catch (error) {
     process.stderr.write(`Post-undeploy failed: ${error.message}\n`);
     process.exitCode = 1;
