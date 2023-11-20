c3306d3344da8627f12b7e2dac08f94cee7c650b
praveenkumarct <praveen.kumar@commercetools.com>
2023-11-20 10:44:32 +0100

    Enable strip implementation in stripe branch

---
+++ order-syncer/src/extensions/stripe/configurations/config.js
@@ -0,0 +1,9 @@
+export function loadConfig() {
+    if (process.env.TAX_PROVIDER_API_TOKEN) {
+        return {
+            taxProviderApiToken: process.env.TAX_PROVIDER_API_TOKEN,
+        };
+    } else {
+        throw new Error('Tax provider API token is not provided.');
+    }
+}
